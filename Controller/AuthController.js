3c4b130a81437c19ee2b46bf870308d4475a52de
govinda <govinda.shinde@gbtech.in>
2024-10-25 10:35:38 +0530

    login auth

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -1,21 +1,40 @@
-const { status } = require('express/lib/response');
-const user = require('./../Models/UserModel');
-const ApiFeatures = require('./../Utils/ApiFeatures');
-const CustomError = require('./../Utils/CustomError');
-const asyncErrorHandler = require('./../Utils/asyncErrorHandler');
-const jwt = require('jsonwebtoken');
+const { status } = require("express/lib/response");
+const user = require("./../Models/UserModel");
+const ApiFeatures = require("./../Utils/ApiFeatures");
+const CustomError = require("./../Utils/CustomError");
+const asyncErrorHandler = require("./../Utils/asyncErrorHandler");
+const jwt = require("jsonwebtoken");
 
 exports.signup = asyncErrorHandler(async (req, res, next) => {
-    const newUser = await user.create(req.body);
-    const token = jwt.sign({id : newUser._id}, process.env.SECRET_STR, {
-        expiresIn : process.env.LOGIN_EXPIRES
-    })
-    res.status(201).json({
-        status: "User Created",
-        token,
-        data: {
-            user: newUser
-        }
-    });
+  const newUser = await user.create(req.body);
+  const token = jwt.sign({ id: newUser._id }, process.env.SECRET_STR, {
+    expiresIn: process.env.LOGIN_EXPIRES,
+  });
+  res.status(201).json({
+    status: "User Created",
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
 
-})
+exports.login = asyncErrorHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  //   let email = req.body.email;
+  //   let password = req.body.password;
+  if (!email || !password) {
+    const error = new CustomError("Invalid email or password", 401);
+    return next(error);
+  }
+  const user1 = await user.findOne({ email }).select("+password");
+  console.log(user1, " user1 ");
+  
+
+  res.status(200).json({
+    status: "User logged in",
+    data: {
+      user: user1,
+    },
+  });
+});
